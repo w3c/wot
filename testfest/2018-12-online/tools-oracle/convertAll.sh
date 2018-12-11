3a3a5ca8c5b90498e243b679a8f28dc6522e4001
Michael Lagally <Michael.Lagally@oracle.com>
2018-12-11 10:59:43 +0100

    update TDs and dm2td.js converter, adding device models

---
+++ testfest/2018-12-online/tools-oracle/convertAll.sh
@@ -0,0 +1,15 @@
+node dm2td.js ../DMs/Blue_Pump.json michaelw3cam7iotjls-michaelprod1.uscom-central-1.Oraclecloud.com 0-AB D78ABF0F-8C7D-44BA-A26F-31EC0073234C >../TDs/Oracle/Blue_Pump_Fujitsu.jsonld
+node dm2td.js ../DMs/Blue_Pump.json michaelw3cam7iotjls-michaelprod1.uscom-central-1.Oraclecloud.com 0-AB C18DE4CF-6537-4FBC-9A9F-49C6EF53E7A1 >../TDs/Oracle/Blue_Pump_Hitachi.jsonld
+node dm2td.js ../DMs/Blue_Pump.json michaelw3cam7iotjls-michaelprod1.uscom-central-1.Oraclecloud.com 0-AB B6EE2F3E-6142-4E0D-99B7-04D84EB93C1E >../TDs/Oracle/Blue_Pump_Intel.jsonld
+node dm2td.js ../DMs/Blue_Pump.json michaelw3cam7iotjls-michaelprod1.uscom-central-1.Oraclecloud.com 0-AB D77F2D20-2A2D-401F-8EC6-9C0EE0615B4C >../TDs/Oracle/Blue_Pump_Panasonic.jsonld
+node dm2td.js ../DMs/Blue_Pump.json michaelw3cam7iotjls-michaelprod1.uscom-central-1.Oraclecloud.com 0-AB 6AF409AF-0E0A-453E-AC8E-E09755C53166 >../TDs/Oracle/Blue_Pump_Siemens.jsonld
+node dm2td.js ../DMs/Blue_Pump.json michaelw3cam7iotjls-michaelprod1.uscom-central-1.Oraclecloud.com 0-AB 10AB2771-2CE0-48DC-B314-492E357C01DD >../TDs/Oracle/Blue_Pump_SmartThings.jsonld
+node dm2td.js ../DMs/Blue_Pump.json michaelw3cam7iotjls-michaelprod1.uscom-central-1.Oraclecloud.com 0-AB 30EF8F7B-787B-4E77-AFAF-B4EAFA09EAA4 >../TDs/Oracle/Blue_Pump_KETI.jsonld
+node dm2td.js ../DMs/Blue_Pump.json michaelw3cam7iotjls-michaelprod1.uscom-central-1.Oraclecloud.com 0-AB E8A25672-6FB6-4AB1-9B58-38DE9A7F4218 >../TDs/Oracle/Blue_Pump_Eurecom.jsonld
+node dm2td.js ../DMs/Connected_Car.json michaelw3cam7iotjls-michaelprod1.uscom-central-1.Oraclecloud.com 0-AB 2566AE46-4C25-4978-886B-A6D75E67F743 >../TDs/Oracle/Connected_Car_Eurecom.jsonld
+node dm2td.js ../DMs/Connected_Car.json michaelw3cam7iotjls-michaelprod1.uscom-central-1.Oraclecloud.com 0-AB 54F510A6-DA22-4732-AB5E-4F53F1DC734F >../TDs/Oracle/Connected_Car_Panasonic.jsonld
+node dm2td.js ../DMs/Connected_Car.json michaelw3cam7iotjls-michaelprod1.uscom-central-1.Oraclecloud.com 0-AB 9F7EBCBB-516C-4C8C-980B-085A71F0F1A2 >../TDs/Oracle/Connected_Car_Shared.jsonld
+node dm2td.js ../DMs/Festo.json michaelw3cam7iotjls-michaelprod1.uscom-central-1.Oraclecloud.com 0-AB CA39BFEA-E0B5-4FBC-A086-CB753AAC0556 >../TDs/Oracle/Festo_Shared.jsonld
+node dm2td.js ../DMs/HVAC_device_model.json michaelw3cam7iotjls-michaelprod1.uscom-central-1.Oraclecloud.com 0-AB C3667123-D2AC-4308-BA42-89F1FC7D7051 >../TDs/Oracle/HVAC_Shared.jsonld
+node dm2td.js ../DMs/ora_obd2_device_model.json michaelw3cam7iotjls-michaelprod1.uscom-central-1.Oraclecloud.com 0-AB B06DB7AF-E388-4FEB-9E38-F83303D179FC >../TDs/Oracle/Truck_Shared.jsonld
+node dm2td.js ../DMs/KETI_IoT_Sensor_Model.json michaelw3cam7iotjls-michaelprod1.uscom-central-1.Oraclecloud.com 0-AB B03198B5-5EE8-4B10-8598-14BE588B0424 >../TDs/Oracle/KETI_IoT_Sensor.jsonld
