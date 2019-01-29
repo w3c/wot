da9e8bd643b88873ecadbe98c080a67f90708a68
Michael McCool <michael.mccool@intel.com>
2019-01-30 02:41:15 +0900

    processing function stubs

--- testfest/2019-02-princeton/update.sh
+++ testfest/2019-02-princeton/update.sh
@@ -8,33 +8,55 @@
 #      TDs will be scanned with AssertionTester, results will be merged with
 #      matching CSV file if it exists, all results will then be merged, results
 #      will be written to outputs/Org/S.csv
-# An implementation description may also be included in the inputs as Org/org.html;
+# Implementation descriptions may also be included in descriptions/Org/org.html;
 # these should include descriptions of all implementations.
-for OrgDir in inputs/*/ ; do
+function process() {
+  Input=$1
+  Output=$2
+  echo "**** Process: $Input -> $Output"
+  # touch $Output
+}
+function merge() {
+  Inputs=$1
+  Output=$2
+  echo "**** Merge: $Inputs -> $Output"
+  # touch $Output
+}
+for OrgDir in inputs/* ; do
   if [[ -d $OrgDir ]]; then
     export AbsOrgDir=$(cd $OrgDir; pwd)
     export Org=$(basename $AbsOrgDir)
-    # echo "Processing organization $Org in $AbsOrgDir"
     echo "Processing organization $Org"
+    echo "  in $AbsOrgDir"
     for ImplPath in $AbsOrgDir/*.jsonld ; do
        if [[ -f $ImplPath ]]; then
-          export Impl=$(basename $ImplPath)
-          # echo "  Processing implementation $Org/$Impl in $ImplPath"
+          export ImplFile=$(basename $ImplPath)
+          export Impl="${ImplFile%.*}"
           echo "  Processing implementation $Org/$Impl"
+          echo "    in $ImplPath"
+          mkdir -p outputs/$Org
+          export AbsOutDir=$(cd outputs/$Org; pwd)
+          process $ImplPath $AbsOutDir/$Impl.csv
        fi
     done
-    for ImplPath in $AbsOrgDir/*/ ; do
+    for ImplPath in $AbsOrgDir/* ; do
        if [[ -d $ImplPath ]]; then
           export Impl=$(basename $ImplPath)
-          # echo "  Processing implementation $Org/$Impl under $ImplPath"
           echo "  Processing implementation $Org/$Impl"
+          echo "    under $ImplPath"
           for InstancePath in $ImplPath/*.jsonld ; do
              if [[ -f $InstancePath ]]; then
-                export Instance=$(basename $InstancePath)
-                # echo "    Processing instance $Org/$Impl/$Instance in $InstancePath"
+                export InstanceFile=$(basename $InstancePath)
+                export Instance="${InstanceFile%.*}"
                 echo "    Processing instance $Org/$Impl/$Instance"
+                echo "      in $InstancePath"
+                mkdir -p outputs/$Org/$Impl
+                export AbsOutDir=$(cd outputs/$Org/$Impl; pwd)
+                process $InstancePath $AbsOutDir/$Instance.csv
              fi
           done
+          mkdir -p outputs/$Org
+          touch outputs/$Org/$Impl.csv
        fi
     done
   fi
