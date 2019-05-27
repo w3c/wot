806f9cf58fd3a8b901bd9aa13b88b242f8121092
Michael Lagally <Michael.Lagally@oracle.com>
2019-05-27 19:11:11 +0200

    improved converter, updated TDs

--- workshop/ws2/demos-2019-Munich/tools/Oracle/gen_TDs.sh
+++ workshop/ws2/demos-2019-Munich/tools/Oracle/gen_TDs.sh
@@ -7,3 +7,5 @@ node dm2td.js ../../TDs/Oracle/DMs/Blue_Pump.json iotpoc8.oracleiotcloud.com 0-A
 node dm2td.js ../../TDs/Oracle/DMs/Blue_Pump.json iotpoc8.oracleiotcloud.com 0-AB 2DF3086C-4FC4-4D32-976F-149A56932E1A > ../../TDs/Oracle/Blue_Pump_Panasonic.jsonld
 node dm2td.js ../../TDs/Oracle/DMs/Blue_Pump.json iotpoc8.oracleiotcloud.com 0-AB 061254FA-9FFD-4DD3-B58A-E3977808C134 > ../../TDs/Oracle/Blue_Pump_Siemens.jsonld
 node dm2td.js ../../TDs/Oracle/DMs/Blue_Pump.json iotpoc8.oracleiotcloud.com 0-AB 5A1289B3-413B-41C3-8CDA-AF74AD7E67F4 > ../../TDs/Oracle/Blue_Pump_SmartThings.jsonld
+node dm2td.js ../../TDs/Oracle/DMs/Festo.json iotpoc8.oracleiotcloud.com 0-AB AE69159D-BED2-4931-BB2D-115865CD1F02 > ../../TDs/Oracle/Festo_Shared.jsonld
+node dm2td.js ../../TDs/Oracle/DMs/Connected_Car.json iotpoc8.oracleiotcloud.com 0-AB 4F5D26A4-FDF8-4D6F-83BA-0CB6989A076A > ../../TDs/Oracle/Connected_Car_Shared.jsonld
