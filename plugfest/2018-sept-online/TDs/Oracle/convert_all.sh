2939d699e29b0baf6f73384d97626ffc7ed4bb44
Michael Lagally <Michael.Lagally@oracle.com>
2019-09-15 10:57:59 +0200

    individual ids

---
+++ plugfest/2018-sept-online/TDs/Oracle/convert_all.sh
@@ -0,0 +1,12 @@
+ #/bin/sh
+ node ../../tools-oracle/dm2td.js Blue_Pump.json 129.150.200.242 0-AB A72C03FE-24A6-413F-9EE5-43AFC0C19F02 > Blue_Pump_Fujitsu.jsonld
+ node ../../tools-oracle/dm2td.js Blue_Pump.json 129.150.200.242 0-AB C7302BA0-22D2-4C22-9381-90A19FC717A2 > Blue_Pump_Panasonic.jsonld
+ node ../../tools-oracle/dm2td.js Blue_Pump.json 129.150.200.242 0-AB 83ED64BF-0882-4077-AA9C-BF90D197F672> Blue_Pump_Hitachi.jsonld
+ node ../../tools-oracle/dm2td.js Blue_Pump.json 129.150.200.242 0-AB 99EB42CC-AEA6-4483-82AC-9D0F7A8982DF > Blue_Pump_Siemens.jsonld
+ node ../../tools-oracle/dm2td.js Blue_Pump.json 129.150.200.242 0-AB 34521F55-6D17-4700-8294-965FF1BE7EF3 > Blue_Pump_SmartThings.jsonld
+ node ../../tools-oracle/dm2td.js Blue_Pump.json 129.150.200.242 0-AB 6E36776A-DF3A-4834-851B-1EFE9C50EDB4 > Blue_Pump_Intel.jsonld
+ node ../../tools-oracle/dm2td.js HVAC_device_model.json 129.150.200.242 0-AB 6B47619E-C4B8-4702-8EFE-DB4F89A7DE75 > HVAC_Shared.jsonld
+ node ../../tools-oracle/dm2td.js Connected_Car.json 129.150.200.242 0-AB 64059FF6-81D8-486E-B87A-D6D6F59F008A > Connected_Car_Shared.jsonld
+ node ../../tools-oracle/dm2td.js ora_obd2_device_model.json 129.150.200.242 0-AB 111AB61F-A5C7-4D5C-BC27-E227C5EE3006 > Truck_Shared.jsonld
+ node ../../tools-oracle/dm2td.js Festo.json 129.150.200.242 0-AB BDF07819-9E73-451B-B58C-FB830E99CD24 > Festo_Shared.jsonld
+ 
